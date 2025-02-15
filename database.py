e90878d5241a2415b4ff48f545afba6edc2de60e
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2025-02-15 08:08:32 +0000

    updated conn

--- database.py
+++ database.py
@@ -1,3 +1,3 @@
 from sqlalchemy import create_engine, text
 
-engine = create_engine('mysql+pymysql://freedb_sandunmadu:&J29vY$xsVrTc#Z@sql.freedb.tech:3306/freedb_educonnect_lms')
+engine = create_engine('mysql+pymysql://freedb_sandunmadu:&J29vY$xsVrTc#Z@sql.freedb.tech:3306/freedb_educonnect_lms', pool_size=10, max_overflow=20)
