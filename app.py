6d2c9b558b3f69d8f29d3627fe376c417462b221
SandunMadhushan <18819020-SandunMadhushan@users.noreply.replit.com>
2025-02-17 19:31:51 +0000

    User checkpoint: Fix: Update database connection string and add mysql-connector-python dependency to resolve connection timeout errors.

--- app.py
+++ app.py
@@ -5,44 +5,44 @@ from sqlalchemy import text
 app = Flask(__name__)
 
 def load_studygroups_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM studygroups"))
-    studygroups = []
-    for row in result.all():
-      studygroups.append(dict(row))
-    return studygroups
+   with engine.connect() as conn:
+     result = conn.execute(text("SELECT * FROM studygroups"))
+     studygroups = []
+     for row in result.all():
+       studygroups.append(dict(row))
+     return studygroups
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+     return render_template("index.html")
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+     return render_template("dashboard.html")
   
 @app.route('/study-groups')
 def studygroups():
-    return render_template('study-groups.html')
+     return render_template('study-groups.html')
 
 # Route for Discussion Forums
 @app.route('/discussion-forums')
 def discussionforums():
-    return render_template('discussion-forums.html')
+     return render_template('discussion-forums.html')
 
 # Route for AI Tools
 @app.route('/ai-tools')
 def aitools():
-    return render_template('ai-tools.html')
+     return render_template('ai-tools.html')
 
 # Route for Study Sessions
 @app.route('/study-sessions')
 def studysessions():
-    return render_template('study-sessions.html')
+     return render_template('study-sessions.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+     app.run(host='0.0.0.0',debug=True)
 
 @app.route('/study-groups/all')
 def ViewStudygroups():
-    studygroups_list = load_studygroups_from_db()
-    return render_template('study-groups-view.html', studygroups=studygroups_list)
+     studygroups_list = load_studygroups_from_db()
+     return render_template('study-groups-view.html', studygroups=studygroups_list)
--- database.py
+++ database.py
@@ -1,3 +1,15 @@
 from sqlalchemy import create_engine, text
 
-engine = create_engine('mysql+pymysql://freedb_sandunmadu:&J29vY$xsVrTc#Z@sql.freedb.tech:3306/freedb_educonnect_lms', pool_size=10, max_overflow=20)
+engine = create_engine('mysql+pymysql://admineduconnectlms:password.2109@educonnectlms.mysql.database.azure.com/educonnect')
+
+# def get_studygroups():
+#     with engine.connect() as conn:
+#         result = conn.execute(text("SELECT * FROM studygroups"))
+#         studygroups = []
+#         for row in result.all():
+#             studygroups.append(dict(row))
+#         return studygroups
+
+with engine.connect() as conn:
+      result = conn.execute(text("SELECT * FROM studygroups"))
+      print(result.all())
