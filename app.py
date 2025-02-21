6ca6a153a5d23fa655f9e8c28c80efd55fe2d6a7
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2025-02-21 10:09:01 +0530

    added custom error pages

--- app.py
+++ app.py
@@ -241,3 +241,15 @@ def extract_text(file_path):
     else:
         with open(file_path, "r", encoding="utf-8") as f:
             return f.read()
+        
+# Custom error pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# Internal server error
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500 
